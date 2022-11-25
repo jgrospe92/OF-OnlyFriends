ef7f3fcc6b4a445f6421ed62b8760879f662a2c9
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2022-11-24 21:42:30 -0500

    add user profile DB
    finish create user table

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,15 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     Button signIn;
+    EditText eText_username, eText_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eText_username = findViewById(R.id.eText_username);
+        eText_password = findViewById(R.id.eText_password);
+
 
         signIn = findViewById(R.id.btn_signIn);
 
@@ -27,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void signUP(View view ) {
+        HashMap<String, String> data = new HashMap<>();
+        Intent intent = new Intent(this.getApplicationContext(), register.class);
+        Toast.makeText(getApplicationContext(), "sigh up" , Toast.LENGTH_SHORT).show();
+        startActivity(intent);
     }
 }
