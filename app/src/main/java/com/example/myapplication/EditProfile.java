54dde1501f222d9e5e751b573a15ef22d1e6718f
G0lG013 <56440164+G0lG013@users.noreply.github.com>
2022-12-08 16:27:54 -0500

    Edit Profile Page has been added
    -EditText for profile name, fname, lname and wallet were implemented to the xml file
    -Button for update, cancel were implemented in the UI

--- app/src/main/java/com/example/myapplication/EditProfile.java
+++ app/src/main/java/com/example/myapplication/EditProfile.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditProfile extends AppCompatActivity {
-
+EditText profNameEditTxt, fnameEditTxt, lnameEditTxt, walletEditTxt;
+Button updateButton, cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        //Defining the EditText in Edit Profile Page
+        profNameEditTxt = findViewById(R.id.profileNameEditText);
+        fnameEditTxt = findViewById(R.id.firstNameEditText);
+        lnameEditTxt = findViewById(R.id.lastNameEditText);
+        walletEditTxt = findViewById(R.id.lastNameEditText);
+
+        //Defining the buttons in Edit Profile Page
+        updateButton = findViewById(R.id.updateAppCompatButton);
+        cancelButton = findViewById(R.id.cancelAppCompatButton);
+
+
+
+
     }
 }
